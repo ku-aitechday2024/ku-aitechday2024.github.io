2c62b82ab8edc4823c5ca14b32be735213c5031e
SeungminCha <dominico1225@gmail.com>
2024-08-08 15:17:24 +0900

    Updates

---
+++ assets/index-3084856c.js
@@ -0,0 +1,11 @@
+import{s as r,B as t}from"./index-028bdcb6.js";const a=r(t)`
+  display: flex;
+  margin-top: 1px;
+  align-items: center;
+  color: ${({theme:o})=>o.palette.primary.main};
+  &:hover {
+    color: ${({theme:o})=>o.palette.secondary.light};
+    cursor: pointer;
+    transition: color 0.2s ease-in-out;
+  }
+`;export{a as L};
